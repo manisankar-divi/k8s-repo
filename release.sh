2b1e5f1faf9e02be8b61b62fb0da4bafc699ef86
manisankar-divi <161689361+manisankar-divi@users.noreply.github.com>
2025-01-30 19:37:02 +0530

    feat: done (#14)
    * workflow added
    
    * workflow added
    
    * workflow added
    
    * workflow added
    
    * workflow added
    
    * workflow added
    
    * workflow added
    
    * workflow added
    
    * workflow added
    
    * workflow added
    
    * workflow added
    
    ---------
    
    Co-authored-by: Mani Sankar <mani.sankar@brightmountainmedia.com>

--- release.sh
+++ release.sh
@@ -54,7 +54,7 @@ for PR in $(echo "$PRS" | jq -r '.[] | select(.merged_at != null) | .number'); d
     "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/pulls/$PR/merge" | jq -r '.sha')
 
   SHORT_COMMIT=${MAIN_COMMIT:0:7} # Extract short commit hash
-  
+
   # Format entry with squash and merge commit ID in the required format
   ENTRY="feat: ${PR_TITLE} (${SHORT_COMMIT}) (#$PR)"
   # Categorize commits
